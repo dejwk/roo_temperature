6c7d4739dcb2126da65e989b807c7c72e3733161
dejwk <dawid.kurzyniec@gmail.com>
2024-05-13 15:57:22 +0200

    Changes to the contract.

--- src/roo_temperature.h
+++ src/roo_temperature.h
@@ -12,9 +12,9 @@ class Temperature {
  public:
   Temperature() : tempC_(std::nanf("")) {}
 
-  float AsK() const { return tempC_ + 273.15; }
-  float AsC() const { return tempC_; }
-  float AsF() const { return tempC_ * 1.8 + 32.0; }
+  float degKelvin() const { return tempC_ + 273.15; }
+  float degCelcius() const { return tempC_; }
+  float degFahrenheit() const { return tempC_ * 1.8 + 32.0; }
 
   bool isUnknown() const { return std::isnan(tempC_); }
 
@@ -43,9 +43,11 @@ class Temperature {
   }
 
  private:
-  friend Temperature TempK(float);
-  friend Temperature TempC(float);
-  friend Temperature TempF(float);
+  friend Temperature Unknown();
+
+  friend Temperature DegKelvin(float);
+  friend Temperature DegCelcius(float);
+  friend Temperature DegFahrenheit(float);
 
   explicit Temperature(float tempC) : tempC_(tempC) {}
 
@@ -54,20 +56,24 @@ class Temperature {
   float tempC_;
 };
 
-inline Temperature TempK(float tempK) { return Temperature(tempK - 273.15); }
+inline Temperature Unknown() { return Temperature(); }
 
-inline Temperature TempC(float tempC) { return Temperature(tempC); }
+inline Temperature DegKelvin(float tempK) {
+  return Temperature(tempK - 273.15);
+}
 
-inline Temperature TempF(float tempF) {
-  return Temperature((tempF - 32.0) / 1.8);
+inline Temperature DegCelcius(float tempC) { return Temperature(tempC); }
+
+inline Temperature DegFahrenheit(float tempF) {
+  return DegCelcius((tempF - 32.0) / 1.8);
 }
 
 inline Temperature operator+(Temperature a, Temperature b) {
-  return TempC(a.AsC() + b.AsC());
+  return DegCelcius(a.degCelcius() + b.degCelcius());
 }
 
 inline Temperature operator-(Temperature a, Temperature b) {
-  return TempC(a.AsC() - b.AsC());
+  return DegCelcius(a.degCelcius() - b.degCelcius());
 }
 
 class Thermometer {
@@ -77,10 +83,40 @@ class Thermometer {
     roo_time::Uptime time;
   };
 
-  virtual ~Thermometer() {}
+  virtual ~Thermometer() = default;
 
   // Returns the last known temperature.
-  virtual Reading getLastReading() const = 0;
+  virtual Reading readTemperature() const = 0;
+};
+
+// Reports readings or another thermometer, if they are fresher than a specified
+// threshold. Otherwise, reports Unknown.
+class CachingThermometer : public Thermometer {
+ public:
+  CachingThermometer(const Thermometer *thermometer)
+      : CachingThermometer(thermometer, roo_time::Hours(10000000)) {}
+
+  CachingThermometer(const Thermometer *thermometer,
+                     roo_time::Interval staleness_threshold)
+      : thermometer_(thermometer), staleness_threshold_(staleness_threshold) {}
+
+  void setStalenessThreshold(roo_time::Interval staleness_threshold) {
+    staleness_threshold_ = staleness_threshold;
+  }
+
+  roo_time::Interval stalenessThreshold() const { return staleness_threshold_; }
+
+  Reading readTemperature() const override {
+    Reading reading = thermometer_->readTemperature();
+    if (reading.time + staleness_threshold_ < roo_time::Uptime::Now()) {
+      reading.value = Unknown();
+    }
+    return reading;
+  }
+
+ private:
+  const Thermometer *thermometer_;
+  roo_time::Interval staleness_threshold_;
 };
 
 }  // namespace roo_temperature
