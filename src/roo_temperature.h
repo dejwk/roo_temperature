da5850ccdc7e4614199be5495315216610f9c8f4
dejwk <dawid.kurzyniec@gmail.com>
2024-08-07 13:39:04 +0200

    Improved comments.

--- src/roo_temperature.h
+++ src/roo_temperature.h
@@ -10,12 +10,19 @@ namespace roo_temperature {
 // degrees.
 class Temperature {
  public:
+  // Creates a temperature object representing an 'unknown' temperature.
   Temperature() : tempC_(std::nanf("")) {}
 
-  float degKelvin() const { return tempC_ + 273.15; }
+  // Returns the temperature in degrees Celcius.
   float degCelcius() const { return tempC_; }
+
+  // Returns the temperature in degrees Kelvin.
+  float degKelvin() const { return tempC_ + 273.15; }
+
+  // Returns the temperature in degrees Fahrenheit.
   float degFahrenheit() const { return tempC_ * 1.8 + 32.0; }
 
+  // Returns whether the object represents an unknown temperature.
   bool isUnknown() const { return std::isnan(tempC_); }
 
   bool operator<(const Temperature &other) const {
@@ -45,8 +52,8 @@ class Temperature {
  private:
   friend Temperature Unknown();
 
-  friend Temperature DegKelvin(float);
   friend Temperature DegCelcius(float);
+  friend Temperature DegKelvin(float);
   friend Temperature DegFahrenheit(float);
 
   explicit Temperature(float tempC) : tempC_(tempC) {}
@@ -55,15 +62,29 @@ class Temperature {
   // temperatures (particularly, zero) behave well when compared for equality.
   float tempC_;
 };
-
+// Returns a temperature object representing an unknown temperature.
 inline Temperature Unknown() { return Temperature(); }
 
+// Returns a temperature object equivalent to the specified temperature
+// expressed in Celcius degrees.
+inline Temperature DegCelcius(float tempC) { return Temperature(tempC); }
+
+// Returns a temperature object equivalent to the specified temperature
+// expressed in Kelvin degrees.
+//
+// Due to floating-point rounding errors, and since the temperature is
+// internally stored in Celcius degrees, generally,
+// DegKelvin(x).degKelvin() != x.
 inline Temperature DegKelvin(float tempK) {
   return Temperature(tempK - 273.15);
 }
 
-inline Temperature DegCelcius(float tempC) { return Temperature(tempC); }
-
+// Returns a temperature object approximately equal to the specified temperature
+// expressed in Fahrenheit degrees.
+//
+// Due to floating-point rounding errors, and since the temperature is
+// internally stored in Celcius degrees, generally,
+// DegFahrenheit(x).degFahrenheit() != x.
 inline Temperature DegFahrenheit(float tempF) {
   return DegCelcius((tempF - 32.0) / 1.8);
 }
@@ -76,8 +97,10 @@ inline Temperature operator-(Temperature a, Temperature b) {
   return DegCelcius(a.degCelcius() - b.degCelcius());
 }
 
+// An abstraction of a termometer, i.e., a device that can report temperature.
 class Thermometer {
  public:
+  // Captures a temperature measured at a specific time.
   struct Reading {
     Temperature value;
     roo_time::Uptime time;
@@ -85,12 +108,12 @@ class Thermometer {
 
   virtual ~Thermometer() = default;
 
-  // Returns the last known temperature.
+  // Returns the latest available temperature reading.
   virtual Reading readTemperature() const = 0;
 };
 
 // Reports readings of another thermometer, if they are fresher than a specified
-// threshold. Otherwise, reports Unknown.
+// expiration threshold. Otherwise, reports Unknown.
 class ExpiringThermometer : public Thermometer {
  public:
   ExpiringThermometer(const Thermometer *thermometer)
@@ -100,7 +123,7 @@ class ExpiringThermometer : public Thermometer {
                       roo_time::Interval expiration)
       : thermometer_(thermometer), expiration_(expiration) {}
 
-  void setExpiratoin(roo_time::Interval expiration) {
+  void setExpiration(roo_time::Interval expiration) {
     expiration_ = expiration;
   }
 
@@ -119,11 +142,10 @@ class ExpiringThermometer : public Thermometer {
   roo_time::Interval expiration_;
 };
 
-// Returns the temperature reading of the specified thermometer, if it is
-// fresher than the specified expiration interval. Otherwise, returns 'unknown'.
-//
-// Use this function if stale thermometer readings (e.g. due to thermometers
-// disconnected from the bus) shouldn't be used.
+// Convenience function that reports the temperature reading of the specified
+// thermometer if it is fresher than the specified expiration threshold, and
+// Unknown otherwise. Use this function if stale thermometer readings (e.g. due
+// to thermometers disconnected from the bus) shouldn't be used.
 inline Thermometer::Reading ReadExpiringTemperature(
     const Thermometer &t, roo_time::Interval expiration) {
   return ExpiringThermometer(&t, expiration).readTemperature();
